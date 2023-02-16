d07af596b4e1592557f0714d3b0bfd6cd5c82946
此心依旧 <1505828047@qq.com>
2023-02-16 07:11:53 +0000

    实验一下

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import re
+
+# url = "http://localhost:7890/pages/31185780/02/31185780/02/resources/osn/alm/topic/alm_r_lof.html"
+# resp = requests.get(url)
+# page_content1 = resp.text
+# 告警解释
+# obj1 = re.compile(r'告警解释</h2>\n<div class="alarmdescbody"><p>(?P<explation>.*?)</p>')
+# data1 = obj1.search(page_content1)
+# print(data1)
+
+f = open("1.txt",mode="r",encoding="utf-8")
+page_content1 = f.read()
+
+# 告警属性
+obj2 = re.compile(r'<td valign="top" headers="mcps1.2.2.1.1"><p>(?P<jibie>.*?)</p>[\s\S]*?2.1.2"><p>(?P<leixing>.*?)</p>')
+data2 = obj2.search(page_content1)
+print(data2.group('jibie'))
+print(data2.group(2))
+
+
+
